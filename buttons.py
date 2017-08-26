bd04076049aea89cb7b19600938dd997ab6b8d30
adamnfish <adamnfish@gmail.com>
2017-08-26 23:53:12 +0100

    Flesh out implementation

--- buttons.py
+++ buttons.py
@@ -1,46 +1,74 @@
 
 class Microbit(object):
-    a_down = False
-    a_waiting = True
-    b_down = False
-    b_waiting = True
+    _a_down = False
+    _a_waiting = True
+    _b_down = False
+    _b_waiting = True
 
-    _microbit = None
+    _running = True
 
-    def init(self, microbit):
-        self._microbit
-        while True:
-            button_logic()
+    _get_time = None
+    _get_a_pressed = None
+    _get_b_pressed = None
 
-    def a_button_state(self):
-        return self._microbit.button_a.is_pressed():
-
-    def b_button_state(self):
-        return self._microbit.button_a.is_pressed():
-
-    def microbit_time(self):
-        return self._microbit.running_time();
-
-    def button_logic(self):
-        a_press = False
-        b_press = False
-        a_down = self.a_button_state()
-        b_down = self.b_button_state()
-        if a_down and a_waiting:
-            a_press = True
+    def init(self, get_time, get_a_pressed, get_b_pressed):
+        """
+        Create instance and provide getters for microbit information.
+        These functions should take no arguments and return an Int (time),
+        or Booleans (button states).
+        """
+        self._get_time = get_time
+        self._get_a_pressed = get_a_pressed
+        self._get_b_pressed = get_b_pressed
         return
 
-    def a_pressed(self):
+    def start(self):
+        while _running:
+            _logic()
+        return
+
+    def stop(self):
+        self._running = False
+        return
+
+    def _logic(self):
+        a_press = False
+        b_press = False
+
+        # ask device for state
+        time = self._get_time()
+        self._a_down = self._get_a_pressed()
+        self._b_down = self._get_b_pressed()
+
+        # detect button presses
+        if self._a_down:
+            if self._a_waiting:
+                a_press = True
+                self._a_waiting = False
+        else:
+            self._a_waiting = True
+        if self._b_down:
+            if self._b_waiting:
+                b_press = True
+                self._b_waiting = False
+        else:
+            self._a_waiting = True
+
+        # run program according to calculated state
+        if a_press:
+            self.a_pressed(time)
+        if b_press:
+            self.b_pressed(time)
+        tick(time, a_press, b_press, self._a_down, self._b_down)
+
+        return
+
+    # implement these to write microbit program
+    def a_pressed(self, time):
         pass
 
-    def b_pressed(self):
+    def b_pressed(self, time):
         pass
 
-    def a_down(self):
-        pass
-
-    def b_down(self):
-        pass
-
-    def tick(self, a_press, b_press, a_down, b_down):
+    def tick(self, time, a_press, b_press, a_down, b_down):
         pass
