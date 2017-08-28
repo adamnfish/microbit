34e40875bbbccbec1f79dccbd1382b1d26925567
adamnfish <adamnfish@gmail.com>
2017-08-28 01:21:40 +0100

    Working dodge game (unpolished)
    Having weird issues with blank screens...

--- dodge.py
+++ dodge.py
@@ -8,7 +8,7 @@ class Dodge(Microbit):
     """
 
     obstacle_row = False
-    field = [([0] * 5)] * 5
+    field = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
     move_count = 0
     move_interval = 1000
     difficulty = 21
@@ -17,16 +17,26 @@ class Dodge(Microbit):
     alive = True
 
     def tick(self, time, a_press, b_press, a_down, b_down):
-        if not self.alive:
-            pass
-        elif self.next_advance_time < time:
+        if self.alive and self.next_advance_time < time:
             self.difficulty = min(self.difficulty + 1, 99)
             self.move_interval = max(self.move_interval - 1, 400)
             self.advance()
             self.display()
-            # self.check_death
+            self.check_death()
             if self.alive:
                 self.next_advance_time = time + self.move_interval
+
+    def a_pressed(self, time):
+        self.player_column = max(0, self.player_column - 1)
+        self.check_death()
+        self.display()
+        return
+
+    def b_pressed(self, time):
+        self.player_column = min(4, self.player_column + 1)
+        self.check_death()
+        self.display()
+        return
 
     def advance(self):
         if self.obstacle_row:
@@ -41,14 +51,19 @@ class Dodge(Microbit):
         self.field[0] = next_row
 
         self.obstacle_row = not self.obstacle_row
-        pass
+        return
+
+    def check_death(self):
+        if self.field[4][self.player_column]:
+            self.alive = False
+        return
 
     def display(self):
         display_str = ""
         for row_index, row in enumerate(self.field):
             display_str += ":"
             for cell_index, cell in enumerate(row):
-                if row_index == 1 and cell_index == self.player_column:
+                if row_index == 4 and cell_index == self.player_column:
                     display_str += "9"
                 elif cell:
                     display_str += "6"
